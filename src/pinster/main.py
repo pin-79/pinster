fd8687e2896b6b30df01125b30cd0c7f16572688
Michał Pindel <m.pindel7991@gmail.com>
2025-01-31 22:36:00 +0100

    feat: switch to library for json logging

--- src/pinster/main.py
+++ src/pinster/main.py
@@ -1,16 +1,18 @@
 """Main pinster module."""
 
+import atexit
+import importlib.resources
+import json
 import logging
 import logging.config
-
-import pinster.logger
+import pathlib
 
 logger = logging.getLogger("pinster")
 
 
 def main() -> None:
     """Main entry point."""
-    pinster.logger.setup_logging()
+    _setup_logging()
     logger.debug("debug msg", extra={"attr": "HELLO"})
     logger.info("debug msg")
     logger.warning("warning msg")
@@ -20,6 +22,23 @@ def main() -> None:
         _ = 1 / 0
     except ZeroDivisionError:
         logger.exception("exception msg")
+
+
+def _setup_logging() -> None:
+    """Sets up the root logger config."""
+    with importlib.resources.open_text("pinster", "configs/logging.json") as f:
+        config = json.load(f)
+
+    # Ensure the logs directory exists
+    log_file = pathlib.Path(config["handlers"]["file"]["filename"])
+    log_file.parent.mkdir(exist_ok=True)
+
+    logging.config.dictConfig(config)
+
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()  # type: ignore[reportUnknownMemberType]
+        atexit.register(queue_handler.listener.stop)  # type: ignore[reportUnknownMemberType]
 
 
 if __name__ == "__main__":
