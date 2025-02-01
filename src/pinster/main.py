d5941de76c258558acc4fde990980a0fe9e5a456
Michał Pindel <m.pindel7991@gmail.com>
2025-02-01 17:56:08 +0100

    feat: download all tracks from a playlist

--- src/pinster/main.py
+++ src/pinster/main.py
@@ -1,13 +1,15 @@
 """Main pinster module."""
 
 import atexit
+import dataclasses
 import importlib.resources
 import json
 import logging
 import logging.config
 import pathlib
-from typing import Annotated
+from typing import Annotated, Any
 
+import rich
 import spotipy
 import typer
 
@@ -16,10 +18,25 @@ logger = logging.getLogger("pinster")
 app = typer.Typer()
 
 
+SPOTIFY_MARKET = "PL"  # ISO 3166-1 alpha-2 country code
+TRACK_FIELDS_TO_RETURN = "id,name,album(release_date),artists(name)"
+
+
+@dataclasses.dataclass(frozen=True)
+class GameTrack:
+    """Track to be used in-game (not representative of the Spotify API response)."""
+
+    id: str
+    name: str
+    artists: list[str]
+    release_date: str
+
+
 @app.command()
 def main(
     spotify_client_id: Annotated[str, typer.Option(prompt=True)],
     spotify_client_secret: Annotated[str, typer.Option(prompt=True)],
+    spotify_redirect_uri: str | None = None,
 ) -> None:
     """Main command."""
     _setup_logging()
@@ -28,11 +45,48 @@ def main(
         auth_manager=spotipy.SpotifyOAuth(
             client_id=spotify_client_id,
             client_secret=spotify_client_secret,
-            redirect_uri="http://localhost:3000",
+            redirect_uri=spotify_redirect_uri or "http://localhost:3000",
             scope="user-library-read, user-modify-playback-state",
         )
     )
-    sp.start_playback(uris=["spotify:track:3qD07JJOPiyqjiQeg1wDK3"])  # type: ignore[reportUnknownMemberType]
+    playlist_tracks = _get_all_playlist_tracks(sp, "21pb7wgr2qqVzKInStmoEm")
+    tracks_to_use: list[GameTrack] = []
+    for track in playlist_tracks:
+        track_data = track["track"]
+        tracks_to_use.append(
+            GameTrack(
+                track_data["id"],
+                track_data["name"],
+                [artist["name"] for artist in track_data["artists"]],
+                track_data["album"]["release_date"],
+            )
+        )
+
+    rich.print(tracks_to_use)
+
+
+def _get_all_playlist_tracks(
+    sp: spotipy.Spotify, playlist_id: str
+) -> list[dict[str, Any]]:
+    """Get all tracks from a playlist."""
+    playlist_tracks: list[dict[str, Any]] = []
+    offset = 0
+    limit = 100
+    total = 1
+    while offset < total:
+        response = sp.playlist_items(
+            playlist_id,
+            fields=f"limit,offset,total,items(track({TRACK_FIELDS_TO_RETURN}))",
+            offset=offset,
+            limit=limit,
+            market=SPOTIFY_MARKET,
+        )
+        if response is None:
+            break
+        playlist_tracks.extend(response["items"])
+        offset += limit
+        total = response["total"]
+    return playlist_tracks
 
 
 def _setup_logging() -> None:
