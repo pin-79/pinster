b79898688924ce21a96d44060657ba28e8249401
pin <33559092+pin-79@users.noreply.github.com>
2025-02-04 17:02:22 +0100

    feat: add basic working game (#2)
    * feat: download all tracks from a playlist
    
    * feat: add main game loop
    
    * feat: replace pause with silence
    
    * feat: change playlist to liked songs
    
    * fix: const typo
    
    * chore: update readme & changelog
    
    * feat: forbid commits to the main branch
    
    * chore: update changelog to new version
    
    * chore: add testpypi index

--- src/pinster/main.py
+++ src/pinster/main.py
@@ -6,8 +6,11 @@ import json
 import logging
 import logging.config
 import pathlib
-from typing import Annotated
+import random
+from typing import Annotated, Any
 
+import rich
+import rich.progress
 import spotipy
 import typer
 
@@ -16,23 +19,71 @@ logger = logging.getLogger("pinster")
 app = typer.Typer()
 
 
+GAME_LIMIT = 100
+SILENCE_PODCAST_EPISODE_ID = "0KgjitRy881dfSEmRhUZE5"
+SPOTIFY_MARKET = "PL"  # ISO 3166-1 alpha-2 country code
+
+
 @app.command()
 def main(
     spotify_client_id: Annotated[str, typer.Option(prompt=True)],
     spotify_client_secret: Annotated[str, typer.Option(prompt=True)],
+    spotify_redirect_uri: str | None = None,
 ) -> None:
     """Main command."""
     _setup_logging()
-
     sp = spotipy.Spotify(
         auth_manager=spotipy.SpotifyOAuth(
             client_id=spotify_client_id,
             client_secret=spotify_client_secret,
-            redirect_uri="http://localhost:3000",
-            scope="user-library-read, user-modify-playback-state",
+            redirect_uri=spotify_redirect_uri or "http://localhost:3000",
+            scope="user-library-read, user-read-playback-state, user-modify-playback-state",
         )
     )
-    sp.start_playback(uris=["spotify:track:3qD07JJOPiyqjiQeg1wDK3"])  # type: ignore[reportUnknownMemberType]
+
+    liked_songs = _get_all_liked_songs_from_api(sp)
+    random.shuffle(liked_songs)
+    typer.confirm(
+        "Track queue ready. Make sure your target device is playing something. Start game?",
+        abort=True,
+    )
+
+    for song in liked_songs[:GAME_LIMIT]:
+        song_data = song["track"]
+        sp.start_playback(uris=[f"spotify:track:{song_data['id']}"])  # type: ignore[reportUnknownMemberType]
+        with rich.progress.Progress(
+            rich.progress.SpinnerColumn(),
+            rich.progress.TextColumn("[progress.description]{task.description}"),
+            transient=True,
+        ) as progress:
+            progress.add_task("Playing track...", total=None)
+            input()
+        sp.start_playback(uris=[f"spotify:episode:{SILENCE_PODCAST_EPISODE_ID}"])  # type: ignore[reportUnknownMemberType]
+        input()
+
+        artists = [artist["name"] for artist in song_data["artists"]]
+        name = song_data["name"]
+        release_date = song_data["album"]["release_date"]
+        rich.print(f"{name}\n{', '.join(artists)}\n{release_date[:4]}")
+        input()
+
+
+def _get_all_liked_songs_from_api(sp: spotipy.Spotify) -> list[dict[str, Any]]:
+    """Get all tracks from a playlist."""
+    liked_songs: list[dict[str, Any]] = []
+    offset = 0
+    limit = 50
+    total = 1
+    while offset < total:
+        response = sp.current_user_saved_tracks(  # type: ignore[reportUnknownMemberType]
+            limit=limit, offset=offset, market=SPOTIFY_MARKET
+        )
+        if response is None:
+            break
+        liked_songs.extend(response["items"])
+        offset += limit
+        total = response["total"]
+    return liked_songs
 
 
 def _setup_logging() -> None:
