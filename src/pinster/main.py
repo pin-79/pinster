e9b6f4a394c52aa3ca23f6e6912b29a46da85f88
Michał Pindel <m.pindel7991@gmail.com>
2025-02-06 22:11:24 +0100

    feat: add building queue from billboard songs

--- src/pinster/main.py
+++ src/pinster/main.py
@@ -36,6 +36,7 @@ def play(
     spotify_client_id: Annotated[str, typer.Option(prompt=True)],
     spotify_client_secret: Annotated[str, typer.Option(prompt=True)],
     spotify_redirect_uri: str | None = None,
+    test: bool = False,  # noqa: FBT001, FBT002
 ) -> None:
     """Runs the game."""
     sp = spotipy.Spotify(
@@ -50,16 +51,26 @@ def play(
         )
     )
 
-    liked_songs = _get_all_liked_songs_from_api(sp)
-    random.shuffle(liked_songs)
+    if test:
+        songs = pinster.billboard.get_songs_with_total_weeks_in_range(10, 15)
+    else:
+        songs = pinster.billboard.get_songs_with_total_weeks_in_range(
+            pinster.billboard.DEFAULT_MIN_WEEKS_THRESHOLD
+        )
+
+    songs = list(songs)
+    random.shuffle(songs)
     typer.confirm(
         "Track queue ready. Make sure your target device is playing something. Start game?",
         abort=True,
     )
 
-    for song in liked_songs[:GAME_LIMIT]:
-        song_data = song["track"]
-        sp.start_playback(uris=[f"spotify:track:{song_data['id']}"])  # type: ignore[reportUnknownMemberType]
+    for song in songs[:GAME_LIMIT]:
+        song_search_result = _get_most_popular_search_result_for_song(sp, song)
+        if not song_search_result:
+            continue
+
+        sp.start_playback(uris=[f"spotify:track:{song_search_result['id']}"])  # type: ignore[reportUnknownMemberType]
         with rich.progress.Progress(
             rich.progress.SpinnerColumn(),
             rich.progress.TextColumn("[progress.description]{task.description}"),
@@ -70,41 +81,26 @@ def play(
         sp.start_playback(uris=[f"spotify:episode:{SILENCE_PODCAST_EPISODE_ID}"])  # type: ignore[reportUnknownMemberType]
         input()
 
-        artists = [artist["name"] for artist in song_data["artists"]]
-        name = song_data["name"]
-        release_date = song_data["album"]["release_date"]
+        artists = [artist["name"] for artist in song_search_result["artists"]]
+        name = song_search_result["name"]
+        release_date = song_search_result["album"]["release_date"]
         rich.print(f"{name}\n{', '.join(artists)}\n{release_date[:4]}")
         input()
 
 
-@app.command()
-def prepare(test: bool = False) -> None:  # noqa: FBT001, FBT002
-    """Prepares the data needed for the game."""
-    if test:
-        songs = pinster.billboard.get_songs_with_total_weeks_in_range(10, 15)
-    else:
-        songs = pinster.billboard.get_songs_with_total_weeks_in_range(
-            pinster.billboard.DEFAULT_MIN_WEEKS_THRESHOLD
-        )
-    rich.print(len(songs))
-
-
-def _get_all_liked_songs_from_api(sp: spotipy.Spotify) -> list[dict[str, Any]]:
-    """Get all tracks from a playlist."""
-    liked_songs: list[dict[str, Any]] = []
-    offset = 0
-    limit = 50
-    total = 1
-    while offset < total:
-        response = sp.current_user_saved_tracks(  # type: ignore[reportUnknownMemberType]
-            limit=limit, offset=offset, market=SPOTIFY_MARKET
-        )
-        if response is None:
-            break
-        liked_songs.extend(response["items"])
-        offset += limit
-        total = response["total"]
-    return liked_songs
+def _get_most_popular_search_result_for_song(
+    sp: spotipy.Spotify, song: pinster.billboard.SimpleSong
+) -> dict[str, Any] | None:
+    search_result = sp.search(  # type: ignore[reportUnknownMemberType]
+        q=f"track:'{song.title}' artist:'{song.artist}'", market=SPOTIFY_MARKET
+    )
+    if not search_result or not search_result.get("tracks", {}).get("items", []):
+        return None
+    return sorted(
+        search_result["tracks"]["items"],
+        key=lambda track: track["popularity"],
+        reverse=True,
+    )[0]
 
 
 if __name__ == "__main__":
